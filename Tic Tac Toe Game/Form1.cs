03b914decb2ffddb016347c5cc2323de70219dc0
thisismic <micmic.inocentes@gmail.com>
2021-05-28 13:39:57 +0800

    Added Disable Buttons and Check For Winner (horizontal, vertical, diagonal)

--- Tic Tac Toe Game/Form1.cs
+++ Tic Tac Toe Game/Form1.cs
@@ -34,6 +34,67 @@ namespace Tic_Tac_Toe_Game
                 b.Text = "O";
             turn = !turn;
             b.Enabled = false;
+            turn_count++;
+            CheckForWinner();
+        }
+
+        private void CheckForWinner()
+        {
+            bool there_is_a_winner = false;
+
+            //horizontal
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            //vertical
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            //diagonal
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == B2.Text) && (B2.Text == A3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            if (there_is_a_winner)
+            {
+                DisableButtons();
+
+                String winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+                MessageBox.Show(winner + " Wins!","Congratulations!");
+            }//end if
+            else
+            {
+                if (turn_count == 9)
+                    MessageBox.Show("It's a Draw!","Wait..");
+            }
+
+
+        }//end CheckForWinner
+
+        private void DisableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }//end foreach
+            }//end try
+            catch { }
         }
     }
 }
