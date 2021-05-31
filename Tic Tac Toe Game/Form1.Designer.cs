27b8a7c71b52188a6efd0b24563882b5234132a6
thisismic <micmic.inocentes@gmail.com>
2021-05-31 13:00:00 +0800

    Designed it more

--- Tic Tac Toe Game/Form1.Designer.cs
+++ Tic Tac Toe Game/Form1.Designer.cs
@@ -29,8 +29,12 @@ namespace Tic_Tac_Toe_Game
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GameMechanics_MenuTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.A1 = new System.Windows.Forms.Button();
             this.B1 = new System.Windows.Forms.Button();
             this.C1 = new System.Windows.Forms.Button();
@@ -40,9 +44,6 @@ namespace Tic_Tac_Toe_Game
             this.A3 = new System.Windows.Forms.Button();
             this.B3 = new System.Windows.Forms.Button();
             this.C3 = new System.Windows.Forms.Button();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.GameMechanics_MenuTool = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -64,123 +65,6 @@ namespace Tic_Tac_Toe_Game
             this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
             this.newGameToolStripMenuItem.Text = "New Game";
             this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
-            // 
-            // A1
-            // 
-            this.A1.BackColor = System.Drawing.SystemColors.Control;
-            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.A1.Location = new System.Drawing.Point(12, 40);
-            this.A1.Name = "A1";
-            this.A1.Size = new System.Drawing.Size(75, 75);
-            this.A1.TabIndex = 1;
-            this.A1.UseVisualStyleBackColor = false;
-            this.A1.Click += new System.EventHandler(this.click_button);
-            this.A1.MouseEnter += new System.EventHandler(this.enter_button);
-            this.A1.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // B1
-            // 
-            this.B1.BackColor = System.Drawing.SystemColors.Control;
-            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.B1.Location = new System.Drawing.Point(93, 40);
-            this.B1.Name = "B1";
-            this.B1.Size = new System.Drawing.Size(75, 75);
-            this.B1.TabIndex = 2;
-            this.B1.UseVisualStyleBackColor = false;
-            this.B1.Click += new System.EventHandler(this.click_button);
-            this.B1.MouseEnter += new System.EventHandler(this.enter_button);
-            this.B1.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // C1
-            // 
-            this.C1.BackColor = System.Drawing.SystemColors.Control;
-            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.C1.Location = new System.Drawing.Point(174, 40);
-            this.C1.Name = "C1";
-            this.C1.Size = new System.Drawing.Size(75, 75);
-            this.C1.TabIndex = 3;
-            this.C1.UseVisualStyleBackColor = false;
-            this.C1.Click += new System.EventHandler(this.click_button);
-            this.C1.MouseEnter += new System.EventHandler(this.enter_button);
-            this.C1.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // A2
-            // 
-            this.A2.BackColor = System.Drawing.SystemColors.Control;
-            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.A2.Location = new System.Drawing.Point(12, 121);
-            this.A2.Name = "A2";
-            this.A2.Size = new System.Drawing.Size(75, 75);
-            this.A2.TabIndex = 4;
-            this.A2.UseVisualStyleBackColor = false;
-            this.A2.Click += new System.EventHandler(this.click_button);
-            this.A2.MouseEnter += new System.EventHandler(this.enter_button);
-            this.A2.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // B2
-            // 
-            this.B2.BackColor = System.Drawing.SystemColors.Control;
-            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.B2.Location = new System.Drawing.Point(93, 121);
-            this.B2.Name = "B2";
-            this.B2.Size = new System.Drawing.Size(75, 75);
-            this.B2.TabIndex = 5;
-            this.B2.UseVisualStyleBackColor = false;
-            this.B2.Click += new System.EventHandler(this.click_button);
-            this.B2.MouseEnter += new System.EventHandler(this.enter_button);
-            this.B2.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // C2
-            // 
-            this.C2.BackColor = System.Drawing.SystemColors.Control;
-            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.C2.Location = new System.Drawing.Point(174, 121);
-            this.C2.Name = "C2";
-            this.C2.Size = new System.Drawing.Size(75, 75);
-            this.C2.TabIndex = 6;
-            this.C2.UseVisualStyleBackColor = false;
-            this.C2.Click += new System.EventHandler(this.click_button);
-            this.C2.MouseEnter += new System.EventHandler(this.enter_button);
-            this.C2.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // A3
-            // 
-            this.A3.BackColor = System.Drawing.SystemColors.Control;
-            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.A3.Location = new System.Drawing.Point(12, 202);
-            this.A3.Name = "A3";
-            this.A3.Size = new System.Drawing.Size(75, 75);
-            this.A3.TabIndex = 7;
-            this.A3.UseVisualStyleBackColor = false;
-            this.A3.Click += new System.EventHandler(this.click_button);
-            this.A3.MouseEnter += new System.EventHandler(this.enter_button);
-            this.A3.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // B3
-            // 
-            this.B3.BackColor = System.Drawing.SystemColors.Control;
-            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.B3.Location = new System.Drawing.Point(93, 202);
-            this.B3.Name = "B3";
-            this.B3.Size = new System.Drawing.Size(75, 75);
-            this.B3.TabIndex = 8;
-            this.B3.UseVisualStyleBackColor = false;
-            this.B3.Click += new System.EventHandler(this.click_button);
-            this.B3.MouseEnter += new System.EventHandler(this.enter_button);
-            this.B3.MouseLeave += new System.EventHandler(this.leave_button);
-            // 
-            // C3
-            // 
-            this.C3.BackColor = System.Drawing.SystemColors.Control;
-            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.C3.Location = new System.Drawing.Point(174, 202);
-            this.C3.Name = "C3";
-            this.C3.Size = new System.Drawing.Size(75, 75);
-            this.C3.TabIndex = 9;
-            this.C3.UseVisualStyleBackColor = false;
-            this.C3.Click += new System.EventHandler(this.click_button);
-            this.C3.MouseEnter += new System.EventHandler(this.enter_button);
-            this.C3.MouseLeave += new System.EventHandler(this.leave_button);
             // 
             // helpToolStripMenuItem
             // 
@@ -205,11 +89,129 @@ namespace Tic_Tac_Toe_Game
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.SystemColors.Info;
+            this.A1.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(12, 40);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(75, 75);
+            this.A1.TabIndex = 1;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.click_button);
+            this.A1.MouseEnter += new System.EventHandler(this.enter_button);
+            this.A1.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.SystemColors.Info;
+            this.B1.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(93, 40);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(75, 75);
+            this.B1.TabIndex = 2;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.click_button);
+            this.B1.MouseEnter += new System.EventHandler(this.enter_button);
+            this.B1.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.SystemColors.Info;
+            this.C1.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.Location = new System.Drawing.Point(174, 40);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(75, 75);
+            this.C1.TabIndex = 3;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.click_button);
+            this.C1.MouseEnter += new System.EventHandler(this.enter_button);
+            this.C1.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.SystemColors.Info;
+            this.A2.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(12, 121);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(75, 75);
+            this.A2.TabIndex = 4;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.click_button);
+            this.A2.MouseEnter += new System.EventHandler(this.enter_button);
+            this.A2.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.SystemColors.Info;
+            this.B2.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(93, 121);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(75, 75);
+            this.B2.TabIndex = 5;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.click_button);
+            this.B2.MouseEnter += new System.EventHandler(this.enter_button);
+            this.B2.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.SystemColors.Info;
+            this.C2.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.Location = new System.Drawing.Point(174, 121);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(75, 75);
+            this.C2.TabIndex = 6;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.click_button);
+            this.C2.MouseEnter += new System.EventHandler(this.enter_button);
+            this.C2.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.SystemColors.Info;
+            this.A3.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(12, 202);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(75, 75);
+            this.A3.TabIndex = 7;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.click_button);
+            this.A3.MouseEnter += new System.EventHandler(this.enter_button);
+            this.A3.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.SystemColors.Info;
+            this.B3.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(93, 202);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(75, 75);
+            this.B3.TabIndex = 8;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.click_button);
+            this.B3.MouseEnter += new System.EventHandler(this.enter_button);
+            this.B3.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.SystemColors.Info;
+            this.C3.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.Location = new System.Drawing.Point(174, 202);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(75, 75);
+            this.C3.TabIndex = 9;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.click_button);
+            this.C3.MouseEnter += new System.EventHandler(this.enter_button);
+            this.C3.MouseLeave += new System.EventHandler(this.leave_button);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(266, 294);
             this.Controls.Add(this.C3);
             this.Controls.Add(this.B3);
